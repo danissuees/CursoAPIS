ea076764fa38c7db8031368033dfd58c802dd543
danissuees <goveamartinezd@gmail.com>
2024-10-21 15:20:08 -0600

    3, insane error fixed

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const eventRoutes = require('./routes/eventRoutes');
+// const eventRoutes = require('./routes/eventRoutes');
 
 dotenv.config
 
 const app = express();
 app.use(express.json());
 
-app.use('api/eventos', eventRoutes);
+// app.use('api/eventos', eventRoutes);
 
 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.info('Servidor corriendo en el puerto ${PORT}');
+    console.info('Hola mundo:3');
+    console.info(`Servidor corriendo en el puerto ${PORT}`);
+    
 });
