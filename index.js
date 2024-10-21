41d3e624c6302aeca672f66d48b670e93f613acc
danissuees <goveamartinezd@gmail.com>
2024-10-21 15:07:00 -0600

    2, empieza el curso

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const eventRoutes = require('./routes/eventRoutes');
+
+dotenv.config
+
+const app = express();
+app.use(express.json());
+
+app.use('api/eventos', eventRoutes);
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.info('Servidor corriendo en el puerto ${PORT}');
+});
