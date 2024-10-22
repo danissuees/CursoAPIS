ff2d85c31d4f56ab87a0a88c8fce578275b2a43d
danissuees <goveamartinezd@gmail.com>
2024-10-22 16:29:18 -0600

    6, fixed fixed

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,13 +1,84 @@
+let eventos= [
+    {
+        id: 1,
+        nombre: 'Coachella',
+        descripcion: 'Puro Humo'
+    },
+    {
+        id: 2,
+        nombre: 'Cepillin',
+        descripcion: 'El Regreso'
+    },
+    {
+        id: 3,
+        nombre: 'Los Tigres del Norte',
+        descripcion: 'Banda Mexicana'
+    }
+];
+
 exports.getEvento = (req, res) => {
     const{id} = req.params;
-    const eventos= ['evento 1', 'evento2', 'evento3'];
-  
-    res.status(200).json(eventos[id-1]);
-    
+    try{
+        const evento = evento.find(evento => evento.id == id)
+        if(!evento){
+            return res.status(404).json('El evento no existe');
+        }
+    res.status(200).json();
+    }
+    catch(error){
+        res.status(500).json(error.message);
+    }
 }
 
 exports.getEventos = (req, res) => {
-    const eventos= ['evento 1', 'evento2', 'evento3']
-    
     res.status(200).json(eventos);
+}
+
+
+
+
+exports.editEvento = (req,res) =>{
+    const {id} = req.params;
+    const {nombre, descripcion} = req.body;
+
+    try{
+    const evento = eventos.find(evento => evento.id == id);
+    evento.nombre = nombre;
+    evento.descripcion = descripcion;
+
+    console.info(eventos);
+
+    res.status(200).json('Se realizaron cambios en el evento' + id);
+    }
+    catch(error){
+        res.status(404).json('No existe tal evento');
+    }
+}
+exports.createEvento = (req,res) =>{
+    const { nombre, descripcion} = req.body;
+
+    const id = eventos.length + 1;
+    const evento ={
+        id,
+        nombre,
+        descripcion
+    }
+    eventos.push(evento);
+    console.info(eventos);
+    res.status(200).json('Se creo el evento ' + nombre +' con id: '+ id)
+}
+exports.deleteEvento  = (req,res) => {
+    try{
+    const {id} = req.params;
+    const nombre = eventos.find(evento => evento.id ==id).nombre;
+
+    eventos = eventos.filter(evento => evento.id != id);
+
+    console.info(eventos);
+
+    res.status(200).json('Se elimino el evento' + nombre)
+    }
+    catch(error){
+        res.status(404).json('No existe tal evento');
+    }
 }
--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -3,10 +3,16 @@ const router = express.Router();
 
 const{
     getEvento,
-    getEventos
+    getEventos,
+    editEvento,
+    createEvento,
+    deleteEvento
 } = require('../controllers/eventController')
 
 router.get('/', getEventos);
 router.get('/:id', getEvento);
+router.put('/:id', editEvento)
+router.post('/', createEvento);
+router.delete('/:id', deleteEvento);
 module.exports = router;
 
