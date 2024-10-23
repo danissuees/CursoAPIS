9fed803f8debbacc5595756f2d8f1d3ad8620f29
danissuees <goveamartinezd@gmail.com>
2024-10-23 10:29:38 -0600

    7, ya jala mysql connection

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,84 +1,97 @@
-let eventos= [
+const db = require('../config/db.js');
+
+let eventos = [
     {
         id: 1,
-        nombre: 'Coachella',
-        descripcion: 'Puro Humo'
+        nombre: 'Cri cri',
+        descripcion: 'Grillitos cri cri'
     },
     {
         id: 2,
-        nombre: 'Cepillin',
-        descripcion: 'El Regreso'
+        nombre: 'Lapizito',
+        descripcion: 'Payasito'
     },
     {
         id: 3,
         nombre: 'Los Tigres del Norte',
-        descripcion: 'Banda Mexicana'
+        descripcion: 'Banda'
     }
-];
+    ];
 
-exports.getEvento = (req, res) => {
-    const{id} = req.params;
+exports.getEvento = (req,res) =>{
+
+    const {id}= req.params;
     try{
-        const evento = evento.find(evento => evento.id == id)
+        const evento = eventos.find(evento => evento.id ==id);
         if(!evento){
             return res.status(404).json('El evento no existe');
         }
-    res.status(200).json();
+
+        db.query('SELECT  id, nombre, descripcion FROM eventos WHERE id = ?',[id], (err,results)=>{
+            if(err) return res.status(500).json(err);
+            res.status(200).json(results[0]);
+        })
+        //res.status(200).josn();
     }
     catch(error){
         res.status(500).json(error.message);
     }
+    
 }
 
-exports.getEventos = (req, res) => {
-    res.status(200).json(eventos);
+exports.getEventos = (req,res)=>{
+    db.query('SELECT* FROM eventos', (err,results)=>{
+        if(err) return res.status(500).json(err);
+        res.status(200).json(results);
+    })
+    
+    //res.status(200).json(eventos);
 }
 
-
-
-
-exports.editEvento = (req,res) =>{
-    const {id} = req.params;
+exports.editEvento = (req,res) => {
+    const {id} = req.params; 
     const {nombre, descripcion} = req.body;
 
-    try{
-    const evento = eventos.find(evento => evento.id == id);
+    const evento = eventos.find(evento => evento.id ==id)
+
     evento.nombre = nombre;
     evento.descripcion = descripcion;
 
     console.info(eventos);
 
-    res.status(200).json('Se realizaron cambios en el evento' + id);
-    }
-    catch(error){
-        res.status(404).json('No existe tal evento');
-    }
+    res.status(200).json('Se realizaron en el evento ' + id);
 }
-exports.createEvento = (req,res) =>{
-    const { nombre, descripcion} = req.body;
+
+exports.createEvento = (req,res)=>{
+    const {nombre, descripcion} = req.body;
 
     const id = eventos.length + 1;
-    const evento ={
+    const evento = {
         id,
         nombre,
         descripcion
     }
     eventos.push(evento);
     console.info(eventos);
-    res.status(200).json('Se creo el evento ' + nombre +' con id: '+ id)
-}
-exports.deleteEvento  = (req,res) => {
-    try{
-    const {id} = req.params;
-    const nombre = eventos.find(evento => evento.id ==id).nombre;
 
-    eventos = eventos.filter(evento => evento.id != id);
+    
+    res.status(200).json('Se creo el evento ' + nombre + ' con el id: '+ id);
+}
+
+exports.deleteEvento = (req,res) =>{
+    try{
+    const { id } = req.params;
+
+    const nombre = eventos.find(evento => evento.id == id).nombre;
+
+    eventos = eventos.map(evento => evento.id != id);
 
     console.info(eventos);
 
-    res.status(200).json('Se elimino el evento' + nombre)
+    res.status(200).json('Se elimino el evento ' + nombre);
     }
     catch(error){
-        res.status(404).json('No existe tal evento');
+        res.status(400).json(error);
+
     }
 }
--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const eventRoutes = require('./routes/eventRoutes');
+const dotenv = require ('dotenv');
+const eventRoutes = require ('./routes/eventRoutes');
 
-//dotenv.config
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/eventos', eventRoutes);
 
-
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.info('Hola mundo:3');
-    console.info(`Servidor corriendo en el puerto ${PORT}`);
-    
+app.listen(PORT, ()=>{
+    console.info('Hola mundo :3');
+    console.info('Servidor corriendo en el puerto '+ PORT);
 });
